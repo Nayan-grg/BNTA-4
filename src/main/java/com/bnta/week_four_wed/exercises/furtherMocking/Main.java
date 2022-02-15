db674a7e237dba91063e851c59a3d39c698a2fc2
Nayan Gurung <nayangrg.se@gmail.com>
2022-02-15 19:45:21 +0000

    Fixed PersonFileDataAccessService

--- src/main/java/com/bnta/week_four_wed/exercises/furtherMocking/Main.java
+++ src/main/java/com/bnta/week_four_wed/exercises/furtherMocking/Main.java
@@ -5,17 +5,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Person nana=new Person(1,"Nana",22);
-//        PersonDAO personDAO=new PersonDataAccess();
-//        PersonService personService=new PersonService(personDAO);
-//        personService.savePerson(nana);
-//        System.out.println(personService.getPeople());
-//        personService.deletePerson(1);
-//        System.out.println(personService.getPeople());
-
         PersonDAO personDAO1= new PersonFileDataAccessService();
         PersonService personService = new PersonService(personDAO1);
-        Person Nayan = new Person(2,"Nayan",23);
-        System.out.println(personService.getPeople());
+//        personService.savePerson(nana);
+//        System.out.println(personService.getPeople());;
+//        System.out.println(personService.getPersonById(1));;
+//        personService.deletePerson(1);
+
+
 
 
 
--- src/main/java/com/bnta/week_four_wed/exercises/furtherMocking/PersonFileDataAccessService.java
+++ src/main/java/com/bnta/week_four_wed/exercises/furtherMocking/PersonFileDataAccessService.java
@@ -15,11 +15,9 @@ public class PersonFileDataAccessService implements PersonDAO {
             if (!file.exists()) {
                 file.createNewFile();
             }
-
             FileWriter fileWriter = new FileWriter(file, true);
             PrintWriter printWriter = new PrintWriter(fileWriter);
             printWriter.println(person.getId() + "," + person.getName() + "," + person.getAge());
-
             // once you are done writing to the file
             printWriter.flush();
             printWriter.close();
@@ -32,17 +30,49 @@ public class PersonFileDataAccessService implements PersonDAO {
 
     @Override
     public void deletePerson(int id) {
+        File inputFile = new File("src/hello.txt");
+        File tempFile = new File("myTempFile.txt");
+        String currentLine;
+        try {
+            String lineToRemove = null;
+            Scanner scanner=new Scanner(inputFile);
+            while(scanner.hasNext()){
+                String line=scanner.nextLine();
+                String[] lineSplit = line.split(",");
+                if(id==Integer.parseInt(lineSplit[0])){
+                    lineToRemove=line;
+                    break;
+                }
+            }
+
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            while ((currentLine = reader.readLine()) != null) {
+                // trim newline when comparing with lineToRemove
+                String trimmedLine = currentLine.trim();
+                if (trimmedLine.equals(lineToRemove)) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+            writer.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        boolean successful = tempFile.renameTo(inputFile);
+
+
+
+
 
     }
 
 
     @Override
     public List<Person> getPeople() {
-        File file =new File("src/hello.txt");
-        Scanner scanner= null;
         try {
+            File file =new File("src/hello.txt");
             List<Person> people=new ArrayList<>();
-            scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file);
             while(scanner.hasNext()){
                 String[] line = scanner.nextLine().split(",");
                 Person person=new Person(Integer.parseInt(line[0]),line[1], Integer.parseInt(line[2]));
